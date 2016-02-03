6fb145460bf16ff88d4f145ef2d13e18ea0aa950
JWest <JWest@GH-LAP0665.nuance.com>
2016-02-03 22:52:38 +0000

    cw

--- AddGoalGUI.py
+++ AddGoalGUI.py
@@ -1,6 +1,5 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-
 from PlayerDatabase import *
 
 class AddGoal(QMainWindow):
@@ -51,44 +50,63 @@ class AddGoal(QMainWindow):
 		self.widget = QWidget()
 		self.widget.setLayout(self.vlayout3)
 		self.setCentralWidget(self.widget)
-	##Widget setting end
 	
+	#Button Clicking
 		self.btnAdd.clicked.connect(self.btnAdd_pushed)
 		self.btnCancel.clicked.connect(self.btnCancel_pushed)
-	
+	#Adding each players Surname to a combo box
 	def PopulatePlayerComboBox(self):
+		#Retrieving all the player details from the database
 		players = g_database.GetAllPlayers()
 		for player in players:
+			#Adds the players Surname to the combo box
 			self.playerCombo.addItem(player[2])
-			
+	#Adding each match oppositon to a combo box		
 	def PopulateMatchComboBox(self):
+		#Retrieving all the Match details from the database
 		matches = g_database.GetAllMatches()
 		for match in matches:
-			print(match)
+			#Adds the match Opposition to the combo box
 			self.matchCombo.addItem(match[2])
 			
 			
 	def btnAdd_pushed(self):
+		#Assigning 'indexP'(P for Player) to the seleceted index in the combo box
 		indexP= self.playerCombo.currentIndex()
+		#Retrieving all the player details from the database
 		players = g_database.GetAllPlayers()
+		#Assigning 'name' to the surname of the selected player from the combo box
 		name = players[indexP][2]
-		indexM =self.matchCombo.currentIndex()
-		matches = g_database.GetAllMatches()
-		match = matches[indexM][2]
 		
+		#Assigning 'indexM'(M for Match) to the seleceted index in the combo box
+		indexM =self.matchCombo.currentIndex()
+		#Retrieving all the Match details from the database
+		matches = g_database.GetAllMatches()
+		#Assigning 'match' to the oppositon of the selected match from the combo box
+		match = matches[indexM][2]
+		#Validation
+		#Assigning 'quantity' to the inputted quantity as text
 		quantity = self.quantity.text()
+		#Checking its not blank
 		if quantity != "":
+			#Running the 'AddGoals' function in the database and adding the data inputted by the user
 			g_database.AddGoals(match,name,int(quantity))
+			#Showing the window that this window was activated from
 			self.parent.show()
+			#Running the 'refreshTable' function in the parent window
 			self.parent.refreshTable()
+			#Closing this window
 			self.close()
-		else:
+		else:	
+			#Running the Error widget, passing in the message to display
 			self.error = ErrorWindow(self,"You did not enter data into all the required fields")
+			#Showing the error window
 			self.error.show()
+			#Rasing the error window to the front of the screen
 			self.error.raise_()
 
-		
-	
 	def btnCancel_pushed(self):
+		#Showing the window that this window was activated from
 		self.parent.show()
+		#Closing this window
 		self.close()
