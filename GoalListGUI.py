b835a25d29cccaceb15af013396201a52d3f8959
JWest <JWest@GH-LAP0665.nuance.com>
2016-03-01 22:15:22 +0000

    hi

--- GoalListGUI.py
+++ GoalListGUI.py
@@ -8,16 +8,19 @@ class GoalList(QMainWindow):
 		super().__init__(parent)
 		self.parent = parent
 		self.setWindowTitle("Goals")
+		#Setting up buttons
 		self.btnNew = QPushButton("New Goal")
 		self.btnHome = QPushButton("Home")
 		
 		
-		
+		#Widget setting
 		
 		self.table = QTableWidget()
 		
 		self.refreshTable()
+		#Running the function 'btn' when the new button is pushed
 		self.btnNew.clicked.connect(self.btnNew_pushed)
+		#Running the function 'btnHome_pushed' when the home button is pushed
 		self.btnHome.clicked.connect(self.btnHome_pushed)
 		
 		self.VlayoutMAIN = QVBoxLayout()
@@ -36,25 +39,37 @@ class GoalList(QMainWindow):
 		self.setCentralWidget(self.widget)
 		
 	def btnNew_pushed(self):
+		#Calls the AddGoal class
 		self.new_goal = AddGoal(self)
+		#Shows the window
 		self.new_goal.show()
+		#Raises the window to the front of the desktop
 		self.new_goal.raise_()
+		#Hides the current window
 		self.hide()
 		
 	def btnHome_pushed(self):
+		#Shows the parent window
 		self.parent.show()
+		#Closes the current window
 		self.close()
 		
 		
 	def refreshTable(self):
+		#Gets all goals from the database
 		Goals = g_database.GetAllGoals()
+		#Sets the row count to the number of entries 
 		self.table.setRowCount(len(Goals))
+		#Sets the column count to 4
 		self.table.setColumnCount(4)
+		#Labels each of the column headers
 		self.table.setHorizontalHeaderLabels(["Id","Oppositon","Player","Quantity"])
 		row = -1
+		#Adds each item in the database to table under the correct header row by row
 		for Goal in Goals:
 			column = 0
 			row = row+1
 			for field in Goal:
+				#Actually adding the item to the table
 				self.table.setItem(row, column, QTableWidgetItem(str(field)))
 				column = column +1
--- errorMessage.py
+++ errorMessage.py
@@ -6,9 +6,10 @@ import sys
 class ErrorWindow(QMainWindow):
 	def __init__(self,parent,error_message):
 		super().__init__()
-		
+		#Widget setting
 		self.error_message = error_message
 		self.setWindowTitle("Error")
+		#Setting the message to the passed in message from the parent
 		self.message = QLabel(self.error_message)
 		self.ok = QPushButton("OK")
 
@@ -23,10 +24,13 @@ class ErrorWindow(QMainWindow):
 		self.widget.setLayout(self.layout)
 
 		self.setCentralWidget(self.widget)
-
+		#Running the function 'back' if the button is pushed
 		self.ok.clicked.connect(self.back)
 
 	def back(self):
+		#Showing the parent screen
 		self.parent.show()
+		#Raisng the parent screen to the front of the desktop
 		self.parent.raise_()
+		#Closing this window
 		self.close()
